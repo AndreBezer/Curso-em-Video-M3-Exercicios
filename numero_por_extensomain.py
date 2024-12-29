c6fff3670607046a037e75be41af7e1ce75303a3
André Luis <andrelus475@gmail.com>
2024-12-29 15:59:29 -0300

    Delete numero_por_extensomain.py

--- numero_por_extensomain.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Digite um numero entre 0 e 20: "))
-
-extenso = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove",
-           "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove",
-           "vinte")
-
-print("Você digitou o número {}".format(extenso[numero]))
