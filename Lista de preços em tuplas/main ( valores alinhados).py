897fa5c08a42d038c008a75005b3db0d2ce5dac3
André Luis <andrelus475@gmail.com>
2025-01-04 19:10:37 -0300

    Create main ( valores alinhados).py

---
+++ Lista de preços em tuplas/main ( valores alinhados).py
@@ -0,0 +1,6 @@
+lista = ("Lápis", "Boarracha", "Caderno", "Estojo", "Transferidor", "Compasso", "Mochila", "Caneta", "Livro")
+
+preco = ("1.75", "2.00", "15.90",  "25.00", "4,20", "9.99", "120.30", "22.30", "34.90")
+
+for cont in range(0, len(lista)):
+    print(f"{lista[cont]:<20}{preco[cont]}")
