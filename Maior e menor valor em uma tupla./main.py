d4c1777040676c63de4ecab0185117aace2d051a
André Luis <andrelus475@gmail.com>
2025-01-04 09:01:54 -0300

    Create main.py

---
+++ Maior e menor valor em uma tupla./main.py
@@ -0,0 +1,7 @@
+import random
+
+lista = (random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10), )
+
+print(f"Os numeros sorteados foram: {lista}")
+print(f"O maior valor foi {max(lista)}")
+print(f"O menor valor foi {min(lista)}")
